2b35bcc342ca86b512bac6bd5f3d1daaa5886532
nauni404 <naufalnurf10@gmail.com>
2023-06-09 19:05:33 +0700

    Membuat migrasi Tabel admin & user dengan seedernya

--- database/migrations/2023_06_09_114201_admin.php
+++ database/migrations/2023_06_09_114201_admin.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('Admin', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->enum('gender', ['L', 'P']);
-            $table->year('birth_year');
-            $table->boolean('vip')->default(false);
         });
     }
 
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('Admin');
     }
 };
